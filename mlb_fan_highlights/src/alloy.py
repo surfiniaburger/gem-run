70d06d19b12bad4811a33ad6650cb1422bfe45e9
surfiniaburger <jdmasciano2@gmail.com>
2025-01-18 01:14:02 +0100

    added dockerfile

--- mlb_fan_highlights/src/alloy.py
+++ mlb_fan_highlights/src/alloy.py
@@ -297,7 +297,7 @@ async def main():
             database="postgres",
             log_level="INFO",  # Can be set to "DEBUG" for more detailed logs,
             db_user='postgres',
-            db_password='<>'
+            db_password='<password>'
         )
         
         # Example: Find similar players for a specific headshot
